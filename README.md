7126b4d8e29570dbf1dc57e8e52a1b2ff930e32b
Ajwyunsx <3427560235@qq.com>
2026-03-29 15:20:14 +0800

    Initial Luna Game 3D source snapshot

---
+++ README.md
@@ -0,0 +1,55 @@
+# Luna Game 3D <img align="right" width="72" src="idTech4Amm/src/main/res/drawable/icon.png" alt="Luna Game 3D icon" />
+
+<p align="center">
+  <img width="320" src="Q3E/src/main/res/drawable/luna_splash.png" alt="Luna Game 3D artwork" />
+</p>
+
+<p align="center">
+  <img width="900" src="docs/images/luna-game-preview.png" alt="Luna Game 3D screenshot" />
+</p>
+
+Luna Game 3D is an Android port project based on the `idTech4A++` / `Q3E` codebase, customized for `Luna Game` branding, package name migration, direct game launch flow, control migration, and UZDoom-focused runtime integration.
+
+## Project Name
+
+- Main project title: `Luna Game 3D`
+- In-app game label: `Luna Game`
+- Android package: `com.luna.game`
+
+## Author And References
+
+- `Luna Game 3D` / `Luna Game` author: [Ajwyunsx](https://github.com/Ajwyunsx)
+- Luna Game 3D author and maintainer: [Ajwyunsx](https://github.com/Ajwyunsx)
+- Luna Game 3D repository: [Ajwyunsx/Luan-Game-3D-Mobile](https://github.com/Ajwyunsx/Luan-Game-3D-Mobile)
+- Upstream author reference: [glKarin](https://github.com/glKarin)
+- Original Android idTech / Q3E author reference: [n0n3m4](https://github.com/n0n3m4)
+- Upstream reference: [glKarin/com.n0n3m4.diii4a](https://github.com/glKarin/com.n0n3m4.diii4a)
+
+## Current Customization
+
+- Renamed the Android app to `Luna Game 3D`
+- Updated the application ID to `com.luna.game`
+- Added Luna Game themed icon and splash artwork
+- Migrated launcher and runtime flow to enter the game directly
+- Migrated on-device control configuration from the previous package
+- Kept UZDoom-related resources and mobile control integration
+
+## Build
+
+This project uses Android Gradle and includes the `idTech4Amm` app module plus the `Q3E` engine frontend module.
+
+```bash
+gradlew.bat :idTech4Amm:assembleDebug
+```
+
+Generated debug APK example:
+
+```text
+idTech4Amm/build/outputs/apk/debug/idTech4Amm-debug.apk
+```
+
+## Notes
+
+- This repository contains the Luna Game 3D customization layer built on top of upstream open-source engine work.
+- Please keep upstream attribution when redistributing modified builds or source snapshots.
+- If you use this project as a base, keep the `Luna Game 3D` and `Luna Game` project naming clear when referring to this customized fork.
---
+++ idTech4Amm/src/main/java/com/karin/idTech4Amm/sys/Constants.java
@@ -0,0 +1,60 @@
+package com.karin.idTech4Amm.sys;
+
+import com.karin.idTech4Amm.BuildConfig;
+import com.karin.idTech4Amm.lib.DateTimeUtility;
+import com.karin.idTech4Amm.misc.TextHelper;
+
+/**
+ * Constants define
+ */
+public final class Constants
+{
+    public static final int    CONST_UPDATE_RELEASE = 72;
+    public static final String CONST_RELEASE = "2026-03-08"; // 02-12; 05-08
+    public static final String CONST_EMAIL = "beyondk2000@gmail.com";
+    public static final String CONST_DEV = "Karin";
+    public static final String CONST_CODE = "Harmattan";
+    public static final String CONST_APP_NAME = "idTech4A++"; // "DIII4A++";
+    public static final String CONST_NAME = "DOOM III/Quake 4/Prey(2006)/DOOM 3 BFG for Android(Harmattan Edition)";
+	public static final String CONST_MAIN_PAGE = "https://github.com/glKarin/com.n0n3m4.diii4a";
+    public static final String CONST_TIEBA = "https://tieba.baidu.com/p/6825594793";
+	public static final String CONST_DEVELOPER = "https://github.com/glKarin";
+    public static final String CONST_DEVELOPER_XDA = "https://forum.xda-developers.com/member.php?u=10584229";
+    public static final String CONST_DISCORD = "https://discord.gg/KFshBra4kh";
+    public static final String CONST_PACKAGE = "com.karin.idTech4Amm";
+    public static final String CONST_FDROID = "https://f-droid.org/packages/com.karin.idTech4Amm/";
+	public static final String CONST_CHECK_FOR_UPDATE_URL = "https://raw.githubusercontent.com/glKarin/com.n0n3m4.diii4a/master/CHECK_FOR_UPDATE.json";
+    public static final String CONST_LICENSE_URL = "https://raw.githubusercontent.com/glKarin/com.n0n3m4.diii4a/master/LICENSE";
+    public static final String CONST_TESTING_URL = "https://github.com/glKarin/com.n0n3m4.diii4a/releases/tag/android_testing";
+    public static final String CONST_CODE_ALIAS = "Lin Daiyu"; // Natasha; Verena; Caitlyn; Lin Daiyu; Lu Yiping; Whip
+	public static String[] CONST_CHANGES()
+    {
+        return new String[] {
+            "Add `Quadrilateral Cowboy`(ver 1.0.2602252) support, game standalone directory named `qc`, game data directory named `base`. More view in `" + TextHelper.GenLinkText("https://blendogames.com/qc/", "Quadrilateral Cowboy") + "`.",
+            "Add `Icarus Starship Command Simulator`(ver 1.0.9) support, game standalone directory named `icarus`, game data directory named `base`. More view in `" + TextHelper.GenLinkText("https://store.steampowered.com/app/620170/Icarus_Starship_Command_Simulator/", "Icarus Starship Command Simulator") + "`.",
+            "Add new game chooser on launcher.",
+        };
+	};
+
+    public static long GetBuildTimestamp()
+    {
+        return BuildConfig.BUILD_TIMESTAMP;
+    }
+
+    public static int GetBuildSDKVersion()
+    {
+        return BuildConfig.BUILD_SDK_VERSION;
+    }
+
+    public static boolean IsDebug()
+    {
+        return BuildConfig.DEBUG;
+    }
+
+    public static String GetBuildTime(String format)
+    {
+        return DateTimeUtility.Format(GetBuildTimestamp(), format);
+    }
+
+	private Constants() {}
+}
